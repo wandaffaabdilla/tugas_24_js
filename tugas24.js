c5529fc11a193e9f5cfee66918e15575ffba98a1
wan daffa abdilla <wandaffaadilla@gmail.com>
2022-03-07 22:51:01 +0800

    Tugas 24 JS
    Tugas ini kami kumpulkan untuk niomic

---
+++ tugas24.js
@@ -0,0 +1,7 @@
+function tugas24() {
+  const angka = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+  console.log("Sebelumnya :", angka.join());
+  console.log("Ascending :", angka.sort().join());
+  console.log("Descending :", angka.reverse().join());
+}
+tugas24();
